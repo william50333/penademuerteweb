1d08b0f59d6207a490ef7f82679cf35237f6b8d0
william50333 <75436485+william50333@users.noreply.github.com>
2025-03-15 10:28:08 -0600

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,23 @@
+const $body = $("body");
+const $header = $(".page-header");
+const $navCollapse = $(".navbar-collapse");
+const scrollClass = "scroll";
+
+$(window).on("scroll", () => {
+  if (this.matchMedia("(min-width: 992px)").matches) {
+    const scrollY = $(this).scrollTop();
+    scrollY > 0
+      ? $body.addClass(scrollClass)
+      : $body.removeClass(scrollClass);
+  } else {
+    $body.removeClass(scrollClass);
+  }
+});
+
+$(".page-header .nav-link, .navbar-brand").on("click", function(e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
+  $("html, body").animate({
+    scrollTop: $(href).offset().top - 71
+  }, 600);
+});
